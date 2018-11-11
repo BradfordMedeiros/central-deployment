0420d0d70ad5b1b639ef3bb6cabb463c46446e5e
brad medeiros <bradmedeiros0@gmail.com>
2018-11-11 12:21:34 -0800

    -

--- code/start.sh
+++ code/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Start website
 nginx -s quit
 cat ./nginx.conf.template | sed "s|{{CONTENT_FOLDER}}|$(pwd)/static|g" > nginx.conf
 nginx -c $(pwd)/nginx.conf
 
+# Start IRC server
 docker run --name inspircd -p 6667:6667 -e "INSP_NET_NAME=Smugs_Server" -d inspircd/inspircd-docker
