9e271cd0f06efd0ca81d7e002906a432a47a59bf
brad medeiros <bradmedeiros0@gmail.com>
2018-11-10 22:03:42 -0800

    install docker, free permissions for docker

--- code/install.sh
+++ code/install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install nginx
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+apt-get -y install nginx
+
+chmod 777 /var/run/docker.sock 
+
 ./start.sh
