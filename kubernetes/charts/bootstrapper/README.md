04e0a453a1f0c240e746a2e1d5a0644faba3ccb0
brad medeiros <bradmedeiros0@gmail.com>
2019-03-03 22:12:56 -0800

    bootstrapper kub config

---
+++ kubernetes/charts/bootstrapper/README.md
@@ -0,0 +1,8 @@
+This deploys hippo. 
+Hippo is package manager for automate. 
+
+Code:
+https://github.com/BradfordMedeiros/hippo
+
+Dockerhub:
+https://cloud.docker.com/repository/docker/bradfordmedeiros/hippo
--- kubernetes/deploy-all.sh
+++ kubernetes/deploy-all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ./charts/prometheus/prometheus-configmap.yaml
-kubectl apply -f ./charts/prometheus/prometheus-deployment.yaml
-kubectl apply -f ./charts/prometheus/prometheus-service.yaml
+#kubectl apply -f ./charts/prometheus/prometheus-configmap.yaml
+#kubectl apply -f ./charts/prometheus/prometheus-deployment.yaml
+#kubectl apply -f ./charts/prometheus/prometheus-service.yaml
 
 kubectl apply -f ./charts/hippo/hippo-deployment.yaml
 kubectl apply -f ./charts/hippo/hippo-service.yaml
@@ -11,7 +11,10 @@ kubectl apply -f ./charts/home/home-configmap.yaml
 kubectl apply -f ./charts/home/home-deployment.yaml
 kubectl apply -f ./charts/home/home-service.yaml
 
-kubectl apply -f ./charts/irc/irc-deployment.yaml
-kubectl apply -f ./charts/irc/irc-service.yaml
+#kubectl apply -f ./charts/irc/irc-deployment.yaml
+#kubectl apply -f ./charts/irc/irc-service.yaml
+
+kubectl apply -f ./charts/bootstrapper/bootstrapper-deployment.yaml
+kubectl apply -f ./charts/bootstrapper/bootstrapper-service.yaml
 
 kubectl apply -f ./charts/main-ingress.yaml
